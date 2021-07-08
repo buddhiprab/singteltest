7f7e8f46eae4969261401d3d8441b30ab8077283
buddhi <buddhi.1986@gmail.com>
2021-07-08 23:06:50 +0800

    DuckTest

--- src/test/java/com/buddhi/DuckTest.java
+++ src/test/java/com/buddhi/DuckTest.java
@@ -1,28 +1,31 @@
 package com.buddhi;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class ChickenTest {
+class DuckTest {
 
     private static ByteArrayOutputStream outContent;
     private static final PrintStream originalOut = System.out;
     private static final PrintStream originalErr = System.err;
 
-    private static Chicken chicken;
+    private static Duck duck;
 
     @BeforeAll
     public static void init() {
-        chicken = new Chicken();
+        duck = new Duck();
     }
 
     @BeforeEach
     public void initEach() {
-        outContent  = new ByteArrayOutputStream();
+        outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
     }
 
@@ -34,13 +37,13 @@ class ChickenTest {
 
     @Test
     public void testSing() {
-        chicken.sing();
-        assertEquals("Cluck, cluck\n", outContent.toString());
+        duck.sing();
+        assertEquals("Quack , quack\n", outContent.toString());
     }
 
     @Test
     public void testFly() {
-        chicken.fly();
-        assertEquals("", outContent.toString());
+        duck.swim();
+        assertEquals("Duck swimming\n", outContent.toString());
     }
 }
